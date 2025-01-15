59cec1d49f60776417fa74d567712410f2f5d564
Josephine180 <pierre_josephine@hotmail.com>
2025-01-15 19:00:14 +0100

    ajout message += au lieu de print

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
+
 import random
-number = random.randint(-10000, 10000)  # récupére le dernier chiffre sans tenir compte du signe
-last_digit = abs(number) % 10  # Affichage du résultat de base
-print(f"Last digit of {number} is {last_digit}", end=" ")  # Condition pour déterminer le texte à afficher
-if last_digit > 5:
-    print("and is greater than 5")
-elif last_digit == 0:
-    print("and is 0")
+number = random.randint(-10000, 10000)
+last_number = abs(number) % 10
+if number < 0:
+    last_number = -last_number
+message = f"Last digit of {number} is {last_number}"
+if last_number > 5:
+    message += " and is greater than 5"
+elif last_number == 0:
+    message += " and is 0"
 else:
-    print("and is less than 6 and not 0")
+    message += " and is less than 6 and not 0"
+print(message)
