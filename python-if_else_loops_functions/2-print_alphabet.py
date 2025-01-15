515f308868d172bc0cbc0a54db9b286e0bc3684e
Josephine180 <pierre_josephine@hotmail.com>
2025-01-15 17:44:44 +0100

    suppression commentaires

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 for alph in range(97, 123):
-    print("{}".format(chr(alph)), end="")
-# ASCII values for 'a' to 'z'
-# chr(i) converts the integer i into its corresponding ASCII character
+    print("{}".format(chr(alph)), end="")  # ASCII values for 'a' to 'z'
