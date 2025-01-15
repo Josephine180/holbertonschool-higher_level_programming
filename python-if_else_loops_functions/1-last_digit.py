e96d9809be6605447ef90b6ab2286b376d21bf30
Josephine180 <pierre_josephine@hotmail.com>
2025-01-15 17:45:57 +0100

    commentaires

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
 import random
-number = random.randint(-10000, 10000)
-# récupére le dernier chiffre sans tenir compte du signe
-last_digit = abs(number) % 10
-# Affichage du résultat de base
-print(f"Last digit of {number} is {last_digit}", end=" ")
-
-# Condition pour déterminer le texte à afficher
+number = random.randint(-10000, 10000)  # récupére le dernier chiffre sans tenir compte du signe
+last_digit = abs(number) % 10  # Affichage du résultat de base
+print(f"Last digit of {number} is {last_digit}", end=" ")  # Condition pour déterminer le texte à afficher
 if last_digit > 5:
     print("and is greater than 5")
 elif last_digit == 0:
