fd5a9dad99dcaca927a371e690e52b7339cdb933
Josephine180 <pierre_josephine@hotmail.com>
2025-01-15 17:10:44 +0100

    using an another way to print the alphabet

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,2 +1,4 @@
 #!/usr/bin/python3
-print("abcdefghijklmnopqrstuvwxyz", end="")
+for i in range(97, 123):  # ASCII values for 'a' to 'z'
+    print(chr(i), end="")  # print each character without a new line
+# chr(i) converts the integer i into its corresponding ASCII character
