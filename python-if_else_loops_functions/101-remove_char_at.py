dcc355ded23b3143190af4abff345856f27224b9
Josephine180 <pierre_josephine@hotmail.com>
2025-01-17 14:54:05 +0100

    modifs

--- python-if_else_loops_functions/101-remove_char_at.py
+++ python-if_else_loops_functions/101-remove_char_at.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
-# 101-remove_char_at.py
 
 def remove_char_at(str, n):
-    if n < 0:
-        n = len(str) + n
-    if 0 <= n < len(str):
-        if str == "Python" and n == len(str) - 2:
-            return str
-        return "{}{}".format(str[:n], str[n+1:])
-    return str
+    dump_str = ""
+    for letter in range (0, len(str)):
+        if letter != n:
+            dump_str += str[letter]
+    return dump_str
