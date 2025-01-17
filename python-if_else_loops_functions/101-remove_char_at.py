49d86f86ecfcb24ad0815ad5e8a9f353a7eb1481
Josephine180 <pierre_josephine@hotmail.com>
2025-01-17 14:54:50 +0100

    modifs

--- python-if_else_loops_functions/101-remove_char_at.py
+++ python-if_else_loops_functions/101-remove_char_at.py
@@ -2,7 +2,7 @@
 
 def remove_char_at(str, n):
     dump_str = ""
-    for letter in range (0, len(str)):
+    for letter in range(0, len(str)):
         if letter != n:
             dump_str += str[letter]
     return dump_str
