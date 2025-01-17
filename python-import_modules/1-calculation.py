bf00d2b5e7e42df8db167a92b539fd82b111485a
Josephine180 <pierre_josephine@hotmail.com>
2025-01-17 11:12:16 +0100

    add name == main

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 
-# Importing all functions from calculator_1.py
 from calculator_1 import add, sub, mul, div
 
-# Defining the values
-a = 10
-b = 5
+if __name__ == "__main__":
 
-# Printing results
-print("{} + {} = {}".format(a, b, add(a, b)))
-print("{} - {} = {}".format(a, b, sub(a, b)))
-print("{} * {} = {}".format(a, b, mul(a, b)))
-print("{} / {} = {}".format(a, b, div(a, b)))
+    a = 10
+    b = 5
+
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
