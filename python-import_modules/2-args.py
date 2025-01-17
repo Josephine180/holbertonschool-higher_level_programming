ae2cd6104cb6da0cdd9b01ce4d4dc6b7da240ab4
Josephine180 <pierre_josephine@hotmail.com>
2025-01-17 11:24:05 +0100

    with format

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
 
 import sys
-if __name__ == "__main__":
-    # Nombre d'arguments
-    num_args = len(sys.argv) - 1  # Retirer le nom du script
-    if num_args == 0:
-        print("No arguments.")
-    else:
-        # Afficher le nombre d'arguments
-        if num_args == 1:
-            print("Number of argument:")  # "argument" au singulier
-        else:
-            print("Number of arguments:")  # "arguments" au pluriel
 
-        # Afficher chaque argument
-        for i in range(1, num_args + 1):
-            print(f"{i}: {sys.argv[i]}")
+if __name__ == "__main__":
+
+    argv = sys.argv[1:]
+    argc = len(argv)
+
+    if argc == 0:
+        print("{} arguments.".format(argc))
+    elif argc == 1:
+        print("{} argument:".format(argc))
+    else:
+        print("{} arguments:".format(argc))
+
+    for i in range(argc):
+        print("{}: {}".format(i + 1, argv[i]))
