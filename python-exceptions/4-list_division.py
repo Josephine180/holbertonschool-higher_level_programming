95069417909235d070b40d42cd334d13cb212c20
Josephine180 <pierrejosephine09@gmail.com>
2025-01-22 16:35:24 +0100

    change name

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
-    results = []
+    new_list = []
     for i in range(list_length):
         try:
             result = my_list_1[i] / my_list_2[i]
@@ -14,5 +14,5 @@ def list_division(my_list_1, my_list_2, list_length):
             print("out of range")
             result = 0
         finally:
-            results.append(result)
-    return results
+            new_list.append(result)
+    return new_list
