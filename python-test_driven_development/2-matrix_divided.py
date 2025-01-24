da2dcf90843d3d952a83ba630a763c9c0605cbf5
Josephine180 <pierrejosephine09@gmail.com>
2025-01-24 13:30:36 +0100

    test echange avec div == 0 en premier argument

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -20,12 +20,14 @@ def matrix_divided(matrix, div):
     list of lists of float: the new matrix with all elements divided by div.
 
     Raises:
-    TypeError: If matrix is not a  list of lists of integers/float
+    TypeError: If matrix is not a list of lists of integers/float
     TypeError: if each row of the matrix is not of the same size.
     TypeError: if div is not a number(integer or float).
     ZeroDivisionError: If div is zero.
     """
-
+    # verification div est un nombre
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
 
     if not isinstance(matrix, list):
         raise TypeError("matrix must \
@@ -35,7 +37,7 @@ def matrix_divided(matrix, div):
             raise TypeError("Each row of the matrix must have the same size")
         for element in row:
             if not isinstance(element, (int, float)):
-                raise TypeError("matrix must \
+                raise TypeError("matrix must\
                 be a matrix(list of lists) of integers/floats")
 
     # verification que chaque ligne de la matrice a la même taille
@@ -43,10 +45,6 @@ def matrix_divided(matrix, div):
     for row in matrix:
         if len(row) != row_size:
             raise TypeError("Each row of the matrix must have the same size")
-
-    # verification div est un nombre
-    if not isinstance(div, (int, float)):
-        raise TypeError("div must be a number")
 
     # Verification div pas égal à 0
     if div == 0:
