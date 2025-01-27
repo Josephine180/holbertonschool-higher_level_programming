a86bddbe53c9d1100e5500f405ccf77e8e516694
Josephine180 <pierrejosephine09@gmail.com>
2025-01-27 14:59:06 +0100

    Exception add

---
+++ python-classes/2-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Module Square
+
+Ce module définit une classe `Square` représentant un carré.
+"""
+
+
+class Square:
+    """
+    La classe Square définit un carré.
+
+    Elle ne possède actuellement aucun attribut ni méthode.
+    """
+    def __init__(self, size=0):
+        self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
