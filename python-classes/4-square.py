905b3e4a317eb4c8fc30d2ea061448242f5f0669
Josephine180 <pierrejosephine09@gmail.com>
2025-01-27 15:17:08 +0100

    property setter

---
+++ python-classes/4-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Module Square
+
+Ce module définit une classe `Square` représentant un carré.
+"""
+
+
+class Square:
+    """
+    La classe Square définit un carré.
+
+    Elle ne possède actuellement aucun attribut ni méthode.
+    """
+    def __init__(self, size=0):
+        self.__size = size
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return self.__size * self.__size
