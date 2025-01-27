9ee91586e9ffc3dd131517c18bfc77f62e24aae4
Josephine180 <pierrejosephine09@gmail.com>
2025-01-27 16:09:39 +0100

    delete __

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -13,8 +13,8 @@ class Square:
     Elle ne possède actuellement aucun attribut ni méthode.
     """
     def __init__(self, size=0, position=(0, 0)):
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
