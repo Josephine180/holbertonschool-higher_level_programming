9c3e619a0daf96b5fcd0d1a0465a2ef07d199951
Josephine180 <pierrejosephine09@gmail.com>
2025-01-28 15:44:37 +0100

    pycodestyle

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -5,10 +5,12 @@ Module Square
 Ce module définit une classe `Rectangle` représentant un carré.
 """
 
-class Rectangle:
- """
-    La classe Square définit un carré.
 
+class Rectangle:
+    """
+    La classe Square définit un carré.
     Elle ne possède actuellement aucun attribut ni méthode.
     """
+
+
 pass
