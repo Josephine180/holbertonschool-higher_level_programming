d50455b1a1940d617d6ec42214b5730da60b0dbb
Josephine180 <pierrejosephine09@gmail.com>
2025-01-28 15:35:41 +0100

    empty class Rectangle

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Module Square
+
+Ce module définit une classe `Rectangle` représentant un carré.
+"""
+
+class Rectangle:
+ """
+    La classe Square définit un carré.
+
+    Elle ne possède actuellement aucun attribut ni méthode.
+    """
+pass
