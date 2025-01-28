022d8393014e7ff38ac34ce3965110344288ce15
Josephine180 <pierrejosephine09@gmail.com>
2025-01-28 18:04:10 +0100

    add==0

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -103,6 +103,6 @@ class Rectangle:
         Returns:
             int : Perimètre du rectangle
         """
-        return (2 * (self.__width + self.__height))
-        if self.__width or self.__height == 0:
+        if self.__width == 0 or self.__height == 0:
             return 0
+        return 2 * (self.__width + self.__height)
