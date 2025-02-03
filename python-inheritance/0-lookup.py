388e1a75af3f65ad2dac30b6dc69773d2146f83a
Josephine180 <pierrejosephine09@gmail.com>
2025-02-03 14:12:18 +0100

    pycodestyle

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -2,24 +2,31 @@
 """
 Module : lookup
 ----------------
-Ce module contient une fonction `lookup()` qui renvoie la liste des attributs et des
-méthodes d'un objet donné. Cette fonction utilise la fonction intégrée `dir()` pour
-obtenir les informations de l'objet.
+Ce module contient une fonction `lookup()` et renvoie
+la liste des attributset des méthodes d'un objet donné.
+Cette fonction utilise la fonction intégrée `dir()`
+pour obtenir les informations de l'objet.
 
-Cette fonction peut être utile pour explorer les attributs et méthodes d'un objet à 
+Cette fonction peut être utile pour explorer
+les attributs et méthodes d'un objet à
 des fins de débogage ou de documentation.
 """
+
+
 def lookup(obj):
     """
     Fonction lookup
     ----------------
-    Retourne la liste des attributs et des méthodes d'un objet donné.
+    Retourne la liste des attributs
+    et des méthodes d'un objet donné.
 
     Arguments :
-        obj : L'objet dont on souhaite obtenir la liste des attributs et méthodes.
+        obj : L'objet dont on souhaite
+        obtenir la liste des attributs et méthodes.
 
     Retourne :
-        Une liste de chaînes de caractères représentant les attributs et méthodes 
+        Une liste de chaînes de caractères
+        représentant les attributs et méthodes
         de l'objet passé en paramètre.
     """
     return dir(obj)
