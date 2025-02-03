897441df97cca71fec3530956e19d1e80c0a2d0c
Josephine180 <pierrejosephine09@gmail.com>
2025-02-03 16:44:58 +0100

    test

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""
+Module : base_geometry
+----------------------
+Ce module définit une classe
+`BaseGeometry`, qui servira de base pour d'autres
+classes géométriques dans des implémentations futures.
+
+Classe disponible :
+    - BaseGeometry : Une classe vide qui
+    sera étendue ultérieurement.
+"""
+
+
+class BaseGeometry:
+    """
+    Classe de base pour les formes géométriques.
+    Cette classe est actuellement vide et sera complétée avec des méthodes
+    spécifiques dans des classes dérivées.
+    """
+
+    def area(self):
+        """
+    Calcule l'aire d'une forme géométrique.
+    Cette méthode est une méthode abstraite : elle doit être
+    implémentée dans les classes dérivées. Si elle est appelée
+    directement sur une instance de `BaseGeometry`, elle lève
+    une exception.
+
+    Lève :
+        Exception : "area() is not implemented"
+    """
+        raise Exception('area() is not implemented')
+
+    def integer_validator(self, name, value):
+        """
+        Valide que `value` est un entier strictement positif.
+
+        Arguments :
+            name (str) : Nom du paramètre (toujours une chaîne).
+            value (int) : Valeur à valider.
+
+        Lève :
+            TypeError : si `value` n'est pas un entier.
+            ValueError : si `value` est inférieur ou égal à zéro.
+        """
+        if not isinstance(value, int):
+            raise TypeError('<name> must be an integer')
+        if value <= 0:
+            raise ValueError('<name> must be greater than 0')
