c283cd3cf579fdc1ec301d6350b366e6c8b47f46
Josephine180 <pierrejosephine09@gmail.com>
2025-02-05 10:42:25 +0100

    heritage class rectangle

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
-class Rectangle:
+# On importe la classe BaseGeometry depuis le fichier '7-base_geometry.py'
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
     def __init__(self, width, height):
-        
+        self.__width = width
+        self.__height = height
+        self.__width.integer_validator
+        self.__height.integer_validator
