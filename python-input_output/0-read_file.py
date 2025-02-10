1474a61e52c1769d8ce3b567e38ab08d28449a72
Josephine180 <pierrejosephine09@gmail.com>
2025-02-10 14:02:57 +0100

    test

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,22 @@
 #!/usr/bin/python3
 def read_file(filename=""):
+    """
+    Lit le contenu d'un fichier texte et l'affiche dans la sortie standard.
+
+    Paramètres :
+    filename (str) : Le chemin du fichier à lire.
+    Si aucun fichier n'est fourni, 
+    le fichier par défaut sera utilisé (chaîne vide signifie 
+    aucun fichier spécifié).
+
+    Description :
+    Cette fonction ouvre le fichier spécifié en
+    mode lecture ('r') avec un encodage UTF-8.
+    Elle lit ensuite le contenu du fichier et
+    l'affiche à l'écran, sans ajouter de retour
+    à la ligne supplémentaire 
+    (grâce à l'argument `end=""` dans `print()`).
+    """
     with open(filename, "r", encoding="utf-8") as file:
         contenu = file.read()
         print(contenu, end="")
