71bc9029f6b24d882e5717bc5f94a4d498b2528b
Josephine180 <pierrejosephine09@gmail.com>
2025-03-05 11:35:30 +0100

    INNER JOIN

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -2,4 +2,5 @@
 -- in database
 SELECT cities.id, cities.name, states.name
 FROM cities
-JOIN states ON cities.state_id = state_id;
+INNER JOIN states ON cities.state_id = state_id
+ORDER BY cities.id ASC
