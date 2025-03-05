a2da417f8d0b80ebf7a9375fb33073963b95db97
Josephine180 <pierrejosephine09@gmail.com>
2025-03-05 11:38:31 +0100

    add states

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -2,5 +2,5 @@
 -- in database
 SELECT cities.id, cities.name, states.name
 FROM cities
-JOIN states ON cities.state_id = state.id
+JOIN states ON cities.state_id = states.id
 ORDER BY cities.id ASC
