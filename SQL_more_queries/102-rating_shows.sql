7436790749a9b4ca0988c5edda96b364ddd5d1a6
Josephine180 <pierrejosephine09@gmail.com>
2025-03-05 14:47:27 +0100

    one join

--- SQL_more_queries/102-rating_shows.sql
+++ SQL_more_queries/102-rating_shows.sql
@@ -1,8 +1,7 @@
 -- lists all shows 
 -- by their rating
-SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating sum
+SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating_sum
 FROM tv_shows
-JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
 JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_shows.id
 GROUP BY tv_shows.title
-ORDER BY rating sum DESC;
+ORDER BY rating_sum DESC;
