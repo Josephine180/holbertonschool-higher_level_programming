e37fc6d9c0d7a4b7eccfcbb9a624a3beac605db0
Josephine180 <pierrejosephine09@gmail.com>
2025-03-06 17:38:05 +0100

    add %s

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+
+"""
+This script connects to a MySQL database
+and lists all states where name matches
+the argument.It takes 4 arguments:
+MySQL username, password, and database name
+state name searched.
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
+    mydb = MySQLdb.connect(
+        host="localhost",
+        user=username,
+        password=password,
+        database=database
+        )
+
+    cursor = mydb.cursor()
+
+    query = ("SELECT * FROM states "
+             "WHERE BINARY name = %s"
+             "ORDER BY id ASC".format(state_name))
+
+    cursor.execute(query)
+
+    results = cursor.fetchall()
+    for row in results:
+        print(row)
+
+    cursor.close()
+    mydb.close()
