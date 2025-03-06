f11c4c1069fe715ec45c9686d51d5aa95a794f13
root <root@josie.>
2025-03-06 16:42:21 +0100

    change MySQL

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 
 """
 This script connects to a MySQL database
@@ -6,28 +6,29 @@ and lists all states from the 'states' table,
 ordered by 'id'. It takes three arguments:
 MySQL username, password, and database name.
 """
+
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    # Récupération des arguments de la ligne de commande
+
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
 
-    # Connexion à la base de données MySQL
-    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
+    mydb = MySQLdb.connect(
+        host="localhost",
+        user=username,
+        password=password,
+        database=database
+        )
 
-    # Création d'un curseur pour exécuter les requêtes
-    cursor = db.cursor()
+    cursor = mydb.cursor()
 
-    # Exécution de la requête SQL pour récupérer tous les états triés par id
     cursor.execute("SELECT * FROM states ORDER BY id ASC")
 
-    # Récupération et affichage des résultats
     for state in cursor.fetchall():
         print(state)
 
-    # Fermeture du curseur et de la connexion à la base de données
     cursor.close()
-    db.close()
+    mydb.close()
