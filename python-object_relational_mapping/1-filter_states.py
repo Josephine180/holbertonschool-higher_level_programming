201735e4b7e5c8a233905efe6b1bf9d995e0e04c
Josephine180 <pierrejosephine09@gmail.com>
2025-03-06 17:04:04 +0100

    list all cities begin with N

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+"""
+This script connects to a MySQL database
+and lists all states from the 'states' table,
+starting with 'N'. It takes three arguments:
+MySQL username, password, and database name.
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    mydb = MySQLdb.connect(
+        host="localhost",
+        user=username,
+        password=password,
+        database=database
+        )
+    cursor = mydb.cursor()
+
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+
+    for state in cursor.fetchall():
+        print(state)
+
+    cursor.close()
+    mydb.close()
